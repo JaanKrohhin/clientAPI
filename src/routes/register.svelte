b0ecbfda576e2098900ffa295df8e41cfa653042
opilane <opilane@opilane.o>
2022-10-06 13:09:03 +0300

    As a user I can register [Delivers #183362014]

--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -38,10 +38,10 @@
 						<input class="form-control form-control-lg" type="text" required placeholder="Name" bind:value={name}>
 					</fieldset><br>
 					<fieldset class="form-group">
+						<input class="form-control form-control-lg" type="password" required placeholder="Password" bind:value={password}>
 						{#if password.length > 1 && password.length < 6}
 							<sup><div class="alert alert-danger" role="alert">Password too short</div></sup>
 						{/if}
-							<input class="form-control form-control-lg" type="password" required placeholder="Password" bind:value={password}>
 					</fieldset><br>
 					<button class="btn btn-lg btn-primary pull-xs-right" disabled="{password.length < 6}">
 						Sign up
